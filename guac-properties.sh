18b4e23f758f0007c09b894019cd3b5db3791c00
Larson Carter <larsoncarter72404@gmail.com>
2022-12-27 17:56:39 -0600

    Fix Properties Try 2
    I am attempting to fix the properties again.

--- guac-properties.sh
+++ guac-properties.sh
@@ -6,6 +6,19 @@ if ! [ $( id -u ) = 0 ]; then
     exit 1
 fi
 
-cat guacamole.properties /etc/guacamole/guacamole.properties > /etc/guacamole/guacamole.properties
+echo "
+ldap-hostname: dc2-server.vv.local
+ldap-port: 389
+ldap-encryption-method: none
+ldap-search-bind-dn: cn=Tech Account,ou=Technology,dc=vv,dc=local
+ldap-search-bind-password: matt
+ldap-user-base-dn: dc=vv,dc=local
+ldap-username-attribute: sAMAccountName
+ldap-group-base-dn: dc=vv,dc=local
+ldap-group-name-attribute: cn
+ldap-user-search-filter: (objectClass=*)
+
+enable-clipboard-integration: true
+" >> /etc/guacamole/guacamole.properties
 
 echo "Done setting up Apache Guacamole Properties!"
